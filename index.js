091df8c0a97e69ba7e5876104d1c231cda65312d
Cody Barker <codybarker.or@gmail.com>
2022-08-28 16:13:54 -0700

    commit tree 1f23297e357cb08f25044ccf47c61a629ebb4f8c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Cody Barker",
+    streetAddress: "11577 Williams Hwy",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const updatedEmployee = {...obj};
+
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj) {
+    const secondUpdatedEmployee = {...obj};
+
+    delete secondUpdatedEmployee.name;
+
+    return secondUpdatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+
+    return employee;
+}
